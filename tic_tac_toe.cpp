dccbecf8eacf92493b8cb501457b79e661174a09
anuprshetty <anuprshetty@gmail.com>
2015-08-16 17:39:01 +0530

    setup method

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -12,3 +12,18 @@ class TicTacToe {
     string check_for_winner();
     void show_board();
 };
+
+void TicTacToe::setup() {
+    char position = '1';
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            board_helper[i][j] = position++;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            board[i][j] = ' ';
+        }
+    }
+}
