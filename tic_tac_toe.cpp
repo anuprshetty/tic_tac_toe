84c0753a6c8721854f9933969d8eac02ecda32c1
anuprshetty <anuprshetty@gmail.com>
2015-08-20 14:40:30 +0530

    computer_turn method

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -47,3 +47,70 @@ void TicTacToe::your_turn() {
         your_turn();
     }
 }
+
+void TicTacToe::computer_turn() {
+    cout << "\nIt's computer's turn.\n";
+
+    for (int i = 0; i < 3; i++) {
+        if ((board[i][0] != ' ' && board[i][1] != ' ') &&
+            (board[i][0] == board[i][1]) && (board[i][2] == ' ')) {
+            board[i][2] = 'O';
+            return;
+        } else if ((board[i][1] != ' ' && board[i][2] != ' ') &&
+                   (board[i][1] == board[i][2]) && (board[i][0] == ' ')) {
+            board[i][0] = 'O';
+            return;
+        } else if ((board[i][0] != ' ' && board[i][2] != ' ') &&
+                   (board[i][0] == board[i][2]) && (board[i][1] == ' ')) {
+            board[i][1] = 'O';
+            return;
+        } else if ((board[0][i] != ' ' && board[1][i] != ' ') &&
+                   (board[0][i] == board[1][i]) && (board[2][i] == ' ')) {
+            board[2][i] = 'O';
+            return;
+        } else if ((board[1][i] != ' ' && board[2][i] != ' ') &&
+                   (board[1][i] == board[2][i]) && (board[0][i] == ' ')) {
+            board[0][i] = 'O';
+            return;
+        } else if ((board[0][i] != ' ' && board[2][i] != ' ') &&
+                   (board[0][i] == board[2][i]) && (board[1][i] == ' ')) {
+            board[1][i] = 'O';
+            return;
+        }
+    }
+
+    if ((board[0][0] != ' ' && board[1][1] != ' ') &&
+        (board[0][0] == board[1][1]) && (board[2][2] == ' ')) {
+        board[2][2] = 'O';
+        return;
+    } else if ((board[1][1] != ' ' && board[2][2] != ' ') &&
+               (board[1][1] == board[2][2]) && (board[0][0] == ' ')) {
+        board[0][0] = 'O';
+        return;
+    } else if ((board[0][0] != ' ' && board[2][2] != ' ') &&
+               (board[0][0] == board[2][2]) && (board[1][1] == ' ')) {
+        board[1][1] = 'O';
+        return;
+    } else if ((board[0][2] != ' ' && board[1][1] != ' ') &&
+               (board[0][2] == board[1][1]) && (board[2][0] == ' ')) {
+        board[2][0] = 'O';
+        return;
+    } else if ((board[1][1] != ' ' && board[2][0] != ' ') &&
+               (board[1][1] == board[2][0]) && (board[0][2] == ' ')) {
+        board[0][2] = 'O';
+        return;
+    } else if ((board[0][2] != ' ' && board[2][0] != ' ') &&
+               (board[0][2] == board[2][0]) && (board[1][1] == ' ')) {
+        board[1][1] = 'O';
+        return;
+    } else {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    board[i][j] = 'O';
+                    return;
+                }
+            }
+        }
+    }
+}
